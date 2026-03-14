d6304c3ea1f319331a9b8244d10118902b1bbe2c
Pedro <pedroppaf@gmail.com>
2026-03-14 00:05:07 -0300

    projeto de agendamento de horarios

---
+++ src/main/java/com/pedro/agenda_horarios/AgendaHorariosApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.agenda_horarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaHorariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaHorariosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/pedro/agenda_horarios/infrastructure/entity/Agendamento.java
@@ -0,0 +1,27 @@
+package com.pedro.agenda_horarios.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "agendamento")
+public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String servico;
+    private String profissional;
+    private LocalDateTime dataHoraAgendamento;
+    private String cliente;
+    private String telefoneCliente;
+    private LocalDateTime dataInsercao = LocalDateTime.now();
+}
